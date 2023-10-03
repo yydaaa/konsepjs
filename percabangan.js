2efc0eea985d93e56b8ec16ddc2cf9c9f24fee3a
Yuda <115441892+yydaaa@users.noreply.github.com>
2023-10-03 11:13:10 +0700

    percabangan.js

--- percabangan.js
+++ percabangan.js
@@ -1,12 +1,17 @@
-let belanja = 100000
+let belanja = 1000000
 console.log("total belanja: "+belanja)
 
 if (belanja > 100000){
+    diskon = belanja * 0.10
     console.log("selamat anda mendapatkan diskon 10%")
 }
 else if(belanja == 100000){
+    diskon = belanja * 0.05
     console.log("selamat anda mendapatkan diskon 5%")
 }
 else{
     console.log("anda tidak mendapatkan diskon")
 }
+
+let pembayaran = belanja - diskon
+console.log("total pembayaran: "+pembayaran)
